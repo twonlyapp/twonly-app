0de6d40aa4a7faee9ba9022ae27eefd0bbb375b6
otsmr <git@tsmr.eu>
2025-03-21 22:06:13 +0100

    ios version does run in simulator

--- lib/src/providers/api_provider.dart
+++ lib/src/providers/api_provider.dart
@@ -28,11 +28,11 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 /// errors or network changes.
 class ApiProvider {
   final String apiUrl = (kDebugMode)
-      ? "ws://10.99.0.6:3030/api/client"
+      ? "ws://10.99.0.140:3030/api/client"
       : "wss://api.twonly.eu/api/client";
   // ws://api.twonly.eu/api/client
   final String? backupApiUrl = (kDebugMode)
-      ? "ws://10.99.0.6:3030/api/client"
+      ? "ws://10.99.0.140:3030/api/client"
       : "wss://api2.twonly.eu/api/client";
   bool isAuthenticated = false;
   ApiProvider();
