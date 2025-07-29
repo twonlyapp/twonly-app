2bde206a8fa40cbca803d7441e64cc8c0f28e7fe
otsmr <git@tsmr.eu>
2025-07-29 11:19:44 +0200

    update icon when coming from settings

--- lib/src/views/chats/chat_list.view.dart
+++ lib/src/views/chats/chat_list.view.dart
@@ -185,13 +185,16 @@ class _ChatListViewState extends State<ChatListView> {
             },
           ),
           IconButton(
-            onPressed: () {
-              Navigator.push(
+            onPressed: () async {
+              await Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => const SettingsMainView(),
                 ),
               );
+              _user = await getUser();
+              if (!mounted) return;
+              setState(() {});
             },
             icon: const FaIcon(FontAwesomeIcons.gear, size: 19),
           )
